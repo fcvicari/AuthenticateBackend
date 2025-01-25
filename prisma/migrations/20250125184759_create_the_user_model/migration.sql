d69755ee98b6327a7607d5e2654f65d7c5f8deff
Fernando Cezar Vicari <fcvicari@gmail.com>
2025-01-25 16:13:06 -0300

    chore: add and configure prism orm with CockroachDB database

---
+++ prisma/migrations/20250125184759_create_the_user_model/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Users" (
+    "id" STRING NOT NULL,
+    "name" STRING NOT NULL,
+    "email" STRING NOT NULL,
+    "password" STRING NOT NULL,
+    "avatar" STRING,
+    "active" BOOL NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
