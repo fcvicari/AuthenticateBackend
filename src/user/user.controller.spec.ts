3bde2d0f7a074e69bda6cbb726a7048c80ef5268
Fernando Cezar Vicari <fcvicari@gmail.com>
2025-01-27 19:18:34 -0300

    feat: implements the secure route to change user password.

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -62,4 +62,63 @@ describe('UserController Tests', () => {
 
     expect(updateUser.email).toEqual(body.email);
   });
+
+  it('Change password user - user does not exist', async () => {
+    const body = {
+      password: '12345',
+      newPassword: '12345NewPassword',
+    };
+
+    await expect(
+      userController.changePasswordUser(body, 'idNotExists'),
+    ).rejects.toHaveProperty('statusCode', 400);
+  });
+
+  it('Change password user - user inactive', async () => {
+    const body = {
+      password: '12345',
+      newPassword: '12345NewPassword',
+    };
+
+    await expect(
+      userController.changePasswordUser(body, '2'),
+    ).rejects.toHaveProperty('statusCode', 401);
+  });
+
+  it('Change user password - current password and new password are the same', async () => {
+    const body = {
+      password: '12345',
+      newPassword: '12345',
+    };
+
+    await expect(
+      userController.changePasswordUser(body, '1'),
+    ).rejects.toHaveProperty(
+      'message',
+      'The current password and the new password cannot be the same.',
+    );
+  });
+
+  it('Change user password - the current password is invalid', async () => {
+    const body = {
+      password: '123456',
+      newPassword: '12345NewPassword',
+    };
+
+    await expect(
+      userController.changePasswordUser(body, '1'),
+    ).rejects.toHaveProperty('message', 'The current password is not valid.');
+  });
+
+  it('Change user password - success', async () => {
+    const body = {
+      password: '12345',
+      newPassword: '12345NewPassword',
+    };
+
+    const updateUser = await userController.changePasswordUser(body, '1');
+
+    expect(updateUser.id).toEqual('1');
+  });
+
 });
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,16 +3,21 @@ import {
   Controller,
   Param,
   Post,
+  Put,
   UseGuards
 } from '@nestjs/common';
 import { AuthGuard } from '../auth/auth.guard';
 import { UserRepository } from '../repository/user/user.repository';
 import { AppError } from '../utils/app.erro';
 import { PasswordHash } from '../utils/password.hash';
+import { ChangePasswordDTO } from './changepassword.Dto';
 import { UserDTO } from './user.Dto';
 
 const UserNotFound = 'User not found.';
 const UserInactive = 'Inactive user.';
+const InvalidPassword = 'The current password is not valid.';
+const ChangePassword =
+  'The current password and the new password cannot be the same.';
 
 @Controller('user')
 @UseGuards(AuthGuard)
@@ -39,6 +44,48 @@ export class UserController {
     userExists.avatar = avatar ? avatar : null;
     userExists.name = name;
     userExists.email = email;
+    userExists.updatedAt = new Date();
+
+    const userUpdated = await this.user.update({
+      where: { id },
+      data: userExists,
+    });
+
+    return {
+      ...userUpdated,
+      password: undefined,
+    };
+  }
+
+  @Put(':id')
+  async changePasswordUser(
+    @Body() body: ChangePasswordDTO,
+    @Param('id') id: string,
+  ) {
+    const { password, newPassword } = body;
+
+    const userExists = await this.user.getUniqueById({ id });
+    if (!userExists) {
+      throw new AppError(UserNotFound, 400);
+    }
+
+    if (!userExists.active) {
+      throw new AppError(UserInactive, 401);
+    }
+
+    if (password === newPassword) {
+      throw new AppError(ChangePassword, 401);
+    }
+
+    const validPassword = await this.hash.compareHash(
+      password,
+      userExists.password,
+    );
+    if (!validPassword) {
+      throw new AppError(InvalidPassword, 401);
+    }
+
+    userExists.password = await this.hash.generateHash(newPassword);
     userExists.updatedAt = new Date();
 
     const userUpdated = await this.user.update({
